ea4d49b8b1d219798ace52d89ac3a897b9dfc4aa
Stefan Pettersson <stefpet@gmail.com>
2012-05-22 09:52:43 +0200

    Renamed parse into parseItem, added parseData hook. Some refactoring.

--- src/jquery.liveupdatelist.js
+++ src/jquery.liveupdatelist.js
@@ -11,6 +11,7 @@
 			options = {},
 			current = [],
 			timer = null,
+			first = true,
 
 			update = function() {
 				if (queue.length == 0) {
@@ -21,13 +22,15 @@
 
 				// move off item exceeding limit		
 				removeLastElement(function() {
+					// when last item is gone, add next
 					addElement(item);
 				});
 			},
 
 			addElement = function(item) {
-				if (options.parse) {
-					item = options.parse.call(this, item);
+				// invoke user provided item parser if exists
+				if (options.parseItem) {
+					item = options.parseItem.call(this, item);
 				}
 
 				item._element = $(item.content);
@@ -46,6 +49,7 @@
 					return;
 				}
 
+				// apply fade out effect to last item
 				var item = current.pop();
 				item._element
 					.fadeOut("fast", function() {
@@ -55,26 +59,6 @@
 			},
 
 			poll = function() {
-				function pollCallback(data) {
-					latestData = data;
-
-					$.each(data, function(key, item) {						
-						queue.push(item);
-					});
-
-					if (timer) {
-						clearInterval(timer);
-						timer = null;
-					}
-
-					if (data && data.length > 0) {
-						timer = setInterval(
-							update,
-							Math.floor(options.interval / (data.length + 1))
-						);
-					}
-				}
-
 				// parameters to be appended to URL
 				if (typeof options.params == "object") {
 					if (latestData && latestData.length > 0) {
@@ -95,7 +79,39 @@
 				$.getJSON(
 					options.url,
 					params,
-					pollCallback
+					function(data) {
+						// invoke user provided data parser if exists
+						if (options.parseData) {
+							data = options.parseData.call(this, data);
+						}
+	
+						// if first request, only display options.max items
+						if (first) {
+							data = data.slice(0, options.max);
+							first = false;
+						}
+	
+						latestData = data;
+	
+						// push new items on the queue
+						$.each(data, function(key, item) {						
+							queue.push(item);
+						});
+	
+						// clear existing timer
+						if (timer) {
+							clearInterval(timer);
+							timer = null;
+						}
+	
+						// create new timer if there is data
+						if (data && data.length > 0) {
+							timer = setInterval(
+								update,
+								Math.floor(options.interval / (data.length + 1))
+							);
+						}
+					}
 				);
 			};
 
